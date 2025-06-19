a7da5236c7a8d5a97567cf963ae06fd01283fc0e
SeongIl037 <kerect@naver.com>
2025-06-20 01:28:30 +0900

    아이템 작업중

---
+++ Assets/02.Scripts/Item/1.Domain/ItemDTO.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class ItemDTO
+{
+    public readonly EItemType ItemType;
+    public readonly int ItemId;
+    public readonly string ItemName;
+    public readonly string ItemDescription;
+    public readonly int PurchasePrice;
+    public readonly int SellPrice;
+    public readonly Sprite ItemImage;
+
+    public ItemDTO(EItemType itemType, int itemId, string itemName, string itemDescription, int purchasePrice, int sellPrice, Sprite itemImage)
+    {
+        ItemType = itemType;
+        ItemId = itemId;
+        ItemName = itemName;
+        ItemDescription = itemDescription;
+        PurchasePrice = purchasePrice;
+        SellPrice = sellPrice;
+        ItemImage = itemImage;
+        
+    }
+
+    public ItemDTO(Item item)
+    {
+        ItemType = item.ItemType;
+        ItemId = item.ItemId;
+        ItemName = item.ItemName;
+        ItemDescription = item.ItemDescription;
+        PurchasePrice = item.PurchasePrice;
+        SellPrice = item.SellPrice;
+        ItemImage = item.ItemImage;
+    }
+}
+        
+    
